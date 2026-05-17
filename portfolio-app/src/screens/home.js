2b77a037fd62526bd918bc358f5b7bed2e02792b
Ludmylla Dias <68711217+ludddddddy@users.noreply.github.com>
2026-05-17 23:11:08 +0000

    COMIT22

---
+++ portfolio-app/src/screens/home.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+
+import api from '../services/api';
+
+export default function Home() {
+  const [perfil, setPerfil] = useState({});
+
+  useEffect(() => {
+    async function carregarPerfil() {
+      const response = await api.get('/perfil');
+
+      setPerfil(response.data);
+    }
+
+    carregarPerfil();
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+      }}
+    >
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }}>
+        {perfil.nome}
+      </Text>
+
+      <Text style={{ fontSize: 20 }}>
+        {perfil.cargo}
+      </Text>
+
+      <Text style={{ marginTop: 20 }}>
+        {perfil.sobre}
+      </Text>
+    </View>
+  );
+}
